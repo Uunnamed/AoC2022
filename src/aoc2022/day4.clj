5204eb88015477c1bb2231151fa0ec59b8949d02
Aleksey Shipilov <alekseyshipilov@LP-GEA00209.local>
2022-12-04 13:24:47 +0400

    refactoring

--- src/aoc2022/day4.clj
+++ src/aoc2022/day4.clj
@@ -1,32 +1,24 @@
 (ns aoc2022.day4
-  (:require [aoc2022.utils :refer [get-input]]
-            [clojure.string :as str]))
-
-
-(defn ->numpair
-  [spair]
-  (->> (str/split spair #"-")
-       (mapv parse-long)))
+  (:require [aoc2022.utils :refer [get-input]]))
 
 
 (defn get-input-d4
   []
   (->> (get-input 2022 4)
-       (str/split-lines)
-       (mapcat #(str/split % #","))
-       (map ->numpair)
-       (partition 2)))
+       (re-seq #"\d+")
+       (map parse-long)
+       (partition 4)))
 
 
 (defn full-overlap?
-  [[[start-a end-a] [start-b end-b]]]
+  [[start-a end-a start-b end-b]]
   (<= (* (- start-b start-a)
          (- end-b end-a))
       0))
 
 
 (defn overlap?
-  [[[start-a end-a] [start-b end-b]]]
+  [[start-a end-a start-b end-b]]
   (<= (* (- start-b end-a)
          (- end-b start-a))
       0))
